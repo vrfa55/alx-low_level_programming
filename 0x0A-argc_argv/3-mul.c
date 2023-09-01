051ccf7342c6b3577ea0bbaf60ebdc045841c88b
Abdelrahman Arafa <vrfa5503@gmail.com>
2023-09-01 14:23:01 -0700

    b

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,18 @@
 
 int main(int argc, char *argv[])
 {
-  int sum;
+	int sum;
 
-  if (argc == 3)
-    {
-      sum = atoi(argv[1]) * atoi(argv[2]);
-      printf("%d\n", sum);
-    }
-  else
-    {
-      printf("Error\n");
-      return (1);
-    }
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-  return (0);
+	return (0);
 }
